33dacec53b4fd1c5c1ff51db80449eb75dd929ec
briandart <nzukib33@gmail.com>
2023-09-10 09:38:45 +0300

    Flutter AnimateProject

--- lib/adapter_view.dart
+++ lib/adapter_view.dart
@@ -1,12 +1,25 @@
 import 'package:flutter/material.dart';
+import 'package:flutter_animate/flutter_animate.dart';
 
 class AdapterView extends StatelessWidget {
   const AdapterView({Key? key}) : super(key: key);
 
   @override
   Widget build(BuildContext context) {
-    return Container(
-      child: Text('This is the AdapterView'),
+    return Scaffold(
+      appBar: AppBar(title: const Text('This is the AdapterView')),
+      body: ListView.builder(itemBuilder: (context, index){
+        return Animate(
+          effects: const [ScaleEffect(begin: Offset(0.8,0.8))],
+          delay: Duration(milliseconds: 100 * index),//use the index value to stagger
+          child: ListTile(
+            title: Text('Item $index'),
+            subtitle: Text('Subtitle $index'),
+            leading: CircleAvatar(child: Text('$index')),
+            trailing: const Icon(Icons.arrow_back_ios),
+          ),
+        );
+      })
     );
   }
 }
--- lib/everything_view.dart
+++ lib/everything_view.dart
@@ -1,6 +1,8 @@
+import 'dart:math';
 import 'dart:ui';
 
 import 'package:flutter/material.dart';
+import 'package:flutter_animate/flutter_animate.dart';
 
 class EverythingView extends StatelessWidget {
   static FragmentShader? shader;
@@ -9,8 +11,22 @@ class EverythingView extends StatelessWidget {
 
   @override
   Widget build(BuildContext context) {
-    return Container(
-      child: Text('This is the EverythingView'),
+    return Scaffold(
+      appBar: AppBar(title: const Text('Staggered Animations Tip')),
+      body:ListView.builder(
+          itemBuilder: (context, index){
+            return Animate(
+              effects: const[ScaleEffect(begin: Offset(0.8,0.8))],
+              delay: Duration(milliseconds: 100 * min(index, 8)),//use min to cap delay
+              child: ListTile(
+                title: Text('Item $index'),
+                subtitle: Text('Subtitle $index'),
+                leading: CircleAvatar(child: Text('$index')),
+                trailing: const Icon(Icons.arrow_back_ios),
+              ),
+            );
+          }
+      ) ,
     );
   }
 }
